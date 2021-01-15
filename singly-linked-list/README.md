bf5e276d8bec17c5ddad6e1c4c4c889be547223b
Andrew Bae <andy@pwner.kr>
2021-01-15 21:13:24 +0900

    Initial commit

---
+++ singly-linked-list/README.md
@@ -0,0 +1,36 @@
+# Single Linked List
+
+## Build
+```
+$ cmake -B build .
+$ ./run #or cmake --build build
+```
+
+## Result
+```
+index: 0
+fd: 0x7f9f8fc05840
+addr: 0x7f9f8fc05820
+data: fuck
+
+index: 1
+fd: 0x7f9f8fc05860
+addr: 0x7f9f8fc05840
+data: fuck
+
+index: 2
+fd: 0x7f9f8fc05880
+addr: 0x7f9f8fc05860
+data: fuck
+
+index: 3
+fd: 0x7f9f8fc058a0
+addr: 0x7f9f8fc05880
+data: fuck
+
+index: 4
+fd: 0x0
+addr: 0x7f9f8fc058a0
+data: fuck
+
+```
---
+++ singly-linked-list/src/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <unistd.h>
+
+struct Node {
+	struct Node *fd;
+	uint8_t data[10];
+};
+
+static struct Node * node_init()
+{
+	struct Node *node = malloc(sizeof(struct Node));
+	memset(node, (uint8_t)0x00, sizeof(struct Node));	
+	return node;
+}
+
+static struct Node * node_push(struct Node *node_prev, uint8_t *data)
+{
+	// create new node then linking
+	struct Node *node = node_init();
+	memcpy(node->data, data, sizeof(node->data));
+	node_prev->fd = node;
+	return node;
+}
+
+static uint8_t node_info(struct Node * node_head)
+{
+	struct Node *node = node_head->fd;
+	for(uint8_t i = 0; node != (void*)0x00; ++i){
+		printf("index: %d\n", i);
+		printf("fd: %p\n", node->fd);
+		printf("addr: %p\n", node);
+		printf("data: %s\n\n", node->data);
+		node = node->fd;
+	}
+}
+
+int main()
+{
+	struct Node *node_head = node_init();
+	struct Node *node_prev = node_head;
+	
+	uint8_t data[10] = "fuck";
+	for(uint8_t i = 0; i < 5; ++i) {
+		struct Node *node = node_push(node_prev, data);
+		node_prev = node;
+	}
+	node_info(node_head);
+	return 0;
+}
