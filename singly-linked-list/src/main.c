f994d526cf7d57fbf3fa01bebe8bfca5012a7bab
Andrew Bae <andy@pwner.kr>
2021-01-18 21:05:52 +0900

    main.c: Fix node_delete logic error and others

--- singly-linked-list/src/main.c
+++ singly-linked-list/src/main.c
@@ -15,7 +15,8 @@ static struct Node * node_init()
 	return node;
 }
 
-static struct Node * node_push(struct Node *node_prev, uint8_t *data)
+
+static struct Node * node_push(struct Node *node_prev, uint16_t *data)
 {
 	// create new node then linking
 	struct Node *node = node_init();
@@ -35,22 +36,21 @@ static struct Node * node_pop(struct Node *node_head, uint16_t index)
 
 static uint8_t node_delete(struct Node *node_head, uint16_t index) 
 {
-	struct Node *prev_node = node_head->fd; 
-	struct Node *next_node;
-	for(uint16_t i = 0; ((index - 1) != i && i < 0xffff); ++i){
+	struct Node *prev_node = node_head, *node, *next_node;	
+	for(uint16_t i = 0; index != i && i < 0xffff; ++i){
 		prev_node = prev_node->fd;
 	}
 	next_node = (prev_node->fd)->fd;	
+	node = prev_node->fd;
 	prev_node->fd = next_node;
-	memset(prev_node->fd, (uint8_t)0x00, sizeof(struct Node));
-
+	free(node);	
 	return EXIT_SUCCESS;
 }
 
 static uint8_t node_info(struct Node * node_head)
 {
 	struct Node *node = node_head->fd;
-	for(uint8_t i = 0; node != (void*)0x00; ++i){
+	for(uint16_t i = 0; node != (void*)0x00; ++i){
 		printf("index: %d\n", i);
 		printf("fd: %p\n", node->fd);
 		printf("addr: %p\n", node);
@@ -64,15 +64,17 @@ int main()
 {
 	struct Node *node_head = node_init();
 	struct Node *node_prev = node_head;
-	
-	uint8_t data[10] = "fuck";
-	for(uint8_t i = 0; i < 5; ++i) {
+	uint16_t data[10], shit;
+
+	for(uint16_t i = 0; i < 5; ++i) {
+		sprintf(&data, "fuck%d", i);
 		struct Node *node = node_push(node_prev, data);
 		node_prev = node;
 	}
 	node_info(node_head);
 	printf("%p", node_pop(node_head, 0x2));
-	node_delete(node_head, 0x3);
+	scanf("%hd", &shit);
+	node_delete(node_head, shit);
 	node_info(node_head);
 	return 0;
 }
